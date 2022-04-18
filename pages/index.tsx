d1cf95c970f631d2b4be76cd334194b9f49faaec
Yehor Lapko <yehorlapko@zoho.com>
2022-04-18 12:59:14 +0300

    Added MUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { Container, Grid, Typography } from "@mui/material";
 
 const HomePage: NextPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <Container maxWidth="lg">
+      <Grid container justifyItems="center">
+        <Grid item xs={12}>
+          <Typography align="center" variant="h3">
+            Main Page
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default HomePage;
